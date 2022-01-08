71b4256b92e8ac6bf56433ff4af0d9fe089ea50a
cobileacd <cobileacd@gmail.com>
2022-01-08 01:39:27 +0000

    updated ss: seems to be finally working but still needs more tests, figure out the correct sizes of both heaps.

--- src/tests and prototypes/ss.c
+++ src/tests and prototypes/ss.c
@@ -37,75 +37,71 @@ void ss(int n,integer_t p[n],integer_t desired_sum,int b[n]){
     integer_t *upa = (integer_t*)malloc(sizeof(integer_t)*(1LL<<n2a));
     integer_t *upb = (integer_t*)malloc(sizeof(integer_t)*(1LL<<n2b));
 
-    min_heap *lower_heap = Create_min_Heap(1LL<<n1b);
-    max_heap *upper_heap = Create_max_heap(1LL<<n2b);
+    printf("size of lwa -> %lld\n", (1LL<<n1a));
+    printf("size of lwb -> %lld\n", (1LL<<n1b));
+    printf("size of upa -> %lld\n", (1LL<<n2a));
+    printf("size of upb -> %lld\n", (1LL<<n2b));
+
+    min_heap *lower_heap = Create_min_Heap((1LL<<n1b+1));
+    max_heap *upper_heap = Create_max_heap((1LL<<n2b+1));
 
     make_sums(n1a,p,lwa);
     make_sums(n1b,p+n1a,lwb);
     make_sums(n2a,p+(n1a+n1b),upa);
     make_sums(n2b,p+(n1a+n1b+n2a),upb);
     
-    printf("size lwa %d \n",(1LL<<n1a));
-    printf("size lwb %d \n",(1LL<<n1b));
-    printf("size upa %d \n",(1LL<<n2a));
-    printf("size upb %d \n",(1LL<<n2b));
-
     int ia=0;
     int ib=0;
     int ja=(1<<n2a)-1;
     int jb=(1<<n2b)-1;
 
-    
-
     for(ib=0;ib<(1LL << n1b);ib++){
-        //printf("%lld ",lwa[ia]+lwb[ib]);
+        //add(Q', (first(T1), P2))
         add(lower_heap,(lwa[ia]+lwb[ib]));
     }
     ia++;
-    ib = 0;
-
     
     for(jb=(1<<n2b)-1;jb>-1;jb--){
-        //printf("%lld -> %d ",upa[ja]+upb[jb],jb);
+        //add(Q'', (first(T3), P4))
         add_max(upper_heap,upa[ja]+upb[jb]);
     }
     ja--;
-    jb =(1<<n2b)-1;
-
-    
 
     int j;
     int i; 
+    integer_t sum; 
 
-    integer_t sum = poll(lower_heap)+poll_max(upper_heap);
+    while(lower_heap->size != 0 && upper_heap->size != 0){
+        i = peek(lower_heap);
+        j = peek_max(upper_heap);
+        sum = i+j;
 
-    
-    
-    while(sum != desired_sum){
-      if(sum < desired_sum){
-        if(ib > (1LL<<n1b)){
-          ia++;
-          add(lower_heap,lwa[ia]+lwb[0]);
-          ib=1;
+        if (sum == desired_sum) break;
+
+        if (sum < desired_sum){
+            poll(lower_heap);
+            if (ia < (1LL<<n1a)){
+                for(ib=0;ib<(1LL << n1b);ib++){
+                    //add(Q', (first(T1), P2))
+                    add(lower_heap,(lwa[ia]+lwb[ib]));
+                }
+                ia++;
+            }
         }
-        i = poll(lower_heap);
-        printf("poll i -> %llu \n",i);
-        sum = j+i;
-        ib++;
-        continue;
-      }
 
-      if(jb < 0){
-        ja--;
-        add_max(upper_heap,upa[ja]+upb[(1<<n2b)-1]);
-        jb = (1<<n2b)-2;
-      }
-      j  = poll_max(upper_heap);
-      printf("poll j -> %llu \n",j);
-      sum = j+i;
-      jb--;
+        if (sum > desired_sum){
+            poll_max(upper_heap);
+            if (ib>-1){
+                for(jb=(1<<n2b)-1;jb>-1;jb--){
+                    //add(Q'', (first(T3), P4))
+                    add_max(upper_heap,upa[ja]+upb[jb]);
+                }
+                ja--;
+            }
+        }
+
     }
-    printf("%d %d",j,i);
+    printf("Result -> %d %d\n",j,i);
 }
 
 int main(void){
@@ -129,21 +125,13 @@ int main(void){
 
     integer_t p[]  = {1,3,7,11,13,19};
 
-    
-
-   
-
     int n = sizeof(p)/sizeof(integer_t);
 
     integer_t k[1LL << n];
 
     make_sums(n,p,k);
 
-    for(int i = 0;i<(1LL << n);i++){
-      printf("%llu ",k[i]);
-    }
-
-    integer_t desired_sum = 44; //p[3] + p[n-1]
+    integer_t desired_sum = 54; //p[3] + p[n-1]
     int b[n];
 
     ss(n,p,desired_sum,b);
